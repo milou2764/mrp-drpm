564e3663b4a51899a48439cd5d6ed2a1e383aefb
Émilien André <emilien.andre@idakto.com>
2024-10-08 17:04:50 +0100

    disable chat bubble

--- lua/darkrp_config/disabled_defaults.lua
+++ lua/darkrp_config/disabled_defaults.lua
@@ -36,7 +36,7 @@ DarkRP.disabledDefaults["modules"] = {
     ["sleep"]            = false,
     ["fadmin"]           = false,
     ["animations"]       = false,
-    ["chatindicator"]    = false,
+    ["chatindicator"]    = true,
 }
 
 --[[---------------------------------------------------------------------------
